b86862a2e007d72e319aee55ef3874510771ae1d
Uponex <kalin@uponex.com>
2022-03-20 10:34:56 +0200

    Added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,11 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
-
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
+
+
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -42,3 +44,47 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Returnt a hello message"""
+        a_viewset = [
+        'Uses actions (list, create, retrieve, update, partial_update)',
+        'Automatically maps to URLs using Routers',
+        'Provade more with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
